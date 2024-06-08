1cc84eea06b57243322ebc8c4b56e5e664dc3c9a
varel14 <varelmoudio14@gmail.com>
2024-06-08 17:21:20 +0100

    db connected

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	db "github.com/varelx/bank/db/sqlc"
 )
 
@@ -14,35 +14,11 @@ const (
 )
 
 func main() {
-	// var testQueries *Queries
-	// var testDB *pgx.Conn
-	var ctx = context.Background()
-
-	conn, err := pgx.Connect(ctx, connString)
+	conn, err := pgxpool.New(context.Background(), connString)
 	if err != nil {
 		log.Fatal("enable to connect to db")
 	}
 	q := db.New(conn)
-	fmt.Println("hello world")
 
-	store := db.NewStore(conn)
-
-	accounts, err := q.ListAccounts(ctx, db.ListAccountsParams{
-		Limit:  10,
-		Offset: 0,
-	})
-	if err != nil || len(accounts) < 2 {
-		log.Fatal("less than 02 accounts")
-	}
-
-	result, err := store.TransferTx(context.Background(), db.TransferTxParams{
-		FromAccountID: accounts[0].ID,
-		ToAccountID:   accounts[1].ID,
-		Amount:        120,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("ok", result)
+	fmt.Println("db connected", q)
 }
